bc517b5a34f86bcf1b04016c802815ce1521505d
张开朗呀~ <1127530756@qq.com>
2024-09-08 15:12:59 +0800

    deploy

---
+++ assets/todoList_index.md.BuEG1IpO.js
@@ -0,0 +1,22 @@
+import{_ as n,o as i,c as l,I as s,j as e,a5 as r,D as c,p as _,l as p,a as b}from"./chunks/framework._yq8SQnG.js";const y=JSON.parse('{"title":"待办事项 - 待开发","description":"","frontmatter":{},"headers":[],"relativePath":"todoList/index.md","filePath":"todoList/index.md","lastUpdated":1724312588000}'),m={name:"todoList/index.md"},t=a=>(_("data-v-d5c5ecbe"),a=a(),p(),a),h=t(()=>e("h1",{id:"待办事项-待开发",tabindex:"-1"},[b("待办事项 - 待开发 "),e("a",{class:"header-anchor",href:"#待办事项-待开发","aria-label":'Permalink to "待办事项 - 待开发"'},"​")],-1)),u=t(()=>e("br",null,null,-1)),v={class:"details custom-block"},x=t(()=>e("summary",null,"播放个视频",-1)),k=r(`<details class="details custom-block" data-v-d5c5ecbe><summary data-v-d5c5ecbe>待办事项选用</summary><ul class="task-list" data-v-d5c5ecbe><li class="task-list-item" data-v-d5c5ecbe><input type="checkbox" id="cbx_0" disabled="true" data-v-d5c5ecbe><label for="cbx_0" data-v-d5c5ecbe>   markdown-it-task-checkbox</label></li><li class="task-list-item" data-v-d5c5ecbe><input type="checkbox" id="cbx_1" checked="true" disabled="true" data-v-d5c5ecbe><label for="cbx_1" data-v-d5c5ecbe>   emoji</label></li></ul></details><pre class="mt-10" data-v-d5c5ecbe>  ✅ 添加网站音乐播放
+
+  ✅ 待办事项 UI 优化
+
+  ✅ 目前为本地搜索，添加 Algolia 或 ✅ pagefind
+
+  ⬜ 媳妇儿白酒销路打开第 1 步
+
+  ⬜ 添加 RSS 源
+
+  ⬜ 其他成员模块和笔记编写规划
+
+  ⬜ 团队成员墙 UI 优化
+
+  ⬜ 笔记分类规划
+
+  ⬜ 博客添加分类标签和归档
+
+  ⬜ live2d 实现拖拽
+
+  ⬜ 导航实现置顶网站栏目展示
+</pre>`,2);function f(a,S,I,T,g,B){const d=c("BackTop"),o=c("Video");return i(),l("div",{"data-pagefind-body":!0},[s(d),h,u,e("details",v,[x,s(o,{second:3,width:"100%",height:"500px",src:"/video/test.mp4",poster:"https://fastly.jsdelivr.net/gh/autumns-heartless/DrawBoard@main/images/1723873301166%E6%88%AA%E5%9B%BE20231117135101.png"})]),k])}const A=n(m,[["render",f],["__scopeId","data-v-d5c5ecbe"]]);export{y as __pageData,A as default};
