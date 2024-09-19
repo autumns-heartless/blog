c38b9f4ef7b22df9e7a55c91844e87a840c86f0d
autumns-heartless <38639760+autumns-heartless@users.noreply.github.com>
2024-09-19 11:12:02 +0800

    Deploying to gh-pages from @ autumns-heartless/blog@499d94d1bbe23544d1b17dbf5a77c38e823f2625 🚀

---
+++ assets/todoList_index.md.EhkEFhER.lean.js
@@ -0,0 +1,22 @@
+import{_ as i,o as l,c as r,G as t,j as e,a as c,a3 as p,B as s}from"./chunks/framework.sJXuPgJd.js";const B=JSON.parse('{"title":"待办事项 - 待开发","description":"","frontmatter":{},"headers":[],"relativePath":"todoList/index.md","filePath":"todoList/index.md","lastUpdated":1724312588000}'),m={name:"todoList/index.md"},u={class:"details custom-block"};function b(v,a,x,_,k,f){const n=s("BackTop"),d=s("ArticleMetadata"),o=s("Video");return l(),r("div",{"data-pagefind-body":!0},[t(n),a[1]||(a[1]=e("h1",{id:"待办事项-待开发",tabindex:"-1"},[c("待办事项 - 待开发 "),e("a",{class:"header-anchor",href:"#待办事项-待开发","aria-label":'Permalink to "待办事项 - 待开发"'},"​")],-1)),t(d),a[2]||(a[2]=e("br",null,null,-1)),e("details",u,[a[0]||(a[0]=e("summary",null,"播放个视频",-1)),t(o,{second:3,width:"100%",height:"500px",src:"/video/test.mp4",poster:"https://fastly.jsdelivr.net/gh/autumns-heartless/DrawBoard@main/images/1723873301166%E6%88%AA%E5%9B%BE20231117135101.png"})]),a[3]||(a[3]=p(`<details class="details custom-block" data-v-5384a75e><summary data-v-5384a75e>待办事项选用</summary><ul class="task-list" data-v-5384a75e><li class="task-list-item" data-v-5384a75e><input type="checkbox" id="cbx_0" disabled="true" data-v-5384a75e><label for="cbx_0" data-v-5384a75e>   markdown-it-task-checkbox</label></li><li class="task-list-item" data-v-5384a75e><input type="checkbox" id="cbx_1" checked="true" disabled="true" data-v-5384a75e><label for="cbx_1" data-v-5384a75e>   emoji</label></li></ul></details><pre class="mt-10" data-v-5384a75e>  ✅ 添加网站音乐播放
+
+  ✅ 待办事项 UI 优化
+
+  ✅ 目前为本地搜索，添加 Algolia 或 ✅ pagefind
+
+  ⬜ 媳妇儿白酒销路打开第 1 步
+
+  ⬜ 添加 RSS 源
+
+  ⬜ 其他成员模块和笔记编写规划
+
+  ⬜ 团队成员墙 UI 优化
+
+  ⬜ 笔记分类规划
+
+  ⬜ 博客添加分类标签和归档
+
+  ⬜ live2d 实现拖拽
+
+  ⬜ 导航实现置顶网站栏目展示
+</pre>`,2))])}const g=i(m,[["render",b],["__scopeId","data-v-5384a75e"]]);export{B as __pageData,g as default};
