22d20038c0eb1c37ec34a7fdd740062394ae513c
autumns-heartless <38639760+autumns-heartless@users.noreply.github.com>
2024-09-19 14:49:22 +0800

    Deploying to gh-pages from @ autumns-heartless/blog@092c12de7835e793f47f928ccc48eca9f46ac8e7 🚀

---
+++ assets/todoList_index.md.BAoObD_4.lean.js
@@ -0,0 +1,24 @@
+import{_ as o,o as i,c as l,G as a,j as t,a as c,a3 as r,B as d}from"./chunks/framework.BXCE_210.js";const f=JSON.parse('{"title":"待办事项 - 待开发","description":"","frontmatter":{},"headers":[],"relativePath":"todoList/index.md","filePath":"todoList/index.md","lastUpdated":1726725917000}'),p={name:"todoList/index.md"};function m(b,e,_,v,x,k){const n=d("BackTop"),s=d("ArticleMetadata");return i(),l("div",{"data-pagefind-body":!0},[a(n),e[0]||(e[0]=t("h1",{id:"待办事项-待开发",tabindex:"-1"},[c("待办事项 - 待开发 "),t("a",{class:"header-anchor",href:"#待办事项-待开发","aria-label":'Permalink to "待办事项 - 待开发"'},"​")],-1)),a(s),e[1]||(e[1]=r(`<br data-v-e79de039><details class="details custom-block" data-v-e79de039><summary data-v-e79de039>待办事项选用</summary><ul class="task-list" data-v-e79de039><li class="task-list-item" data-v-e79de039><input type="checkbox" id="cbx_0" disabled="true" data-v-e79de039><label for="cbx_0" data-v-e79de039>   markdown-it-task-checkbox</label></li><li class="task-list-item" data-v-e79de039><input type="checkbox" id="cbx_1" checked="true" disabled="true" data-v-e79de039><label for="cbx_1" data-v-e79de039>   emoji</label></li></ul></details><pre class="mt-10" data-v-e79de039>  ✅ 添加网站音乐播放
+
+  ✅ 待办事项 UI 优化
+
+  ✅ 目前为本地搜索，添加 Algolia 或 ✅ pagefind
+
+  ✅ 媳妇儿白酒销路打开第 1 步：广告
+
+  ⬜ 媳妇儿白酒销路打开第 2 步：外部网站
+
+  ⬜ 添加 RSS 源
+
+  ⬜ 其他成员模块和笔记编写规划
+
+  ⬜ 团队成员墙 UI 优化
+
+  ⬜ 笔记分类规划
+
+  ⬜ 博客添加分类标签和归档
+
+  ⬜ live2d 实现拖拽
+
+  ⬜ 导航实现置顶网站栏目展示
+</pre>`,3))])}const h=o(p,[["render",m],["__scopeId","data-v-e79de039"]]);export{f as __pageData,h as default};
