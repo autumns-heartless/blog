66f6a6840a144674cf365641e0a07ac5155c361e
autumns-heartless <38639760+autumns-heartless@users.noreply.github.com>
2024-11-02 10:06:17 +0800

    Deploying to gh-pages from @ autumns-heartless/blog@aaa5e444304a4ef1cd1c058a7fccd41528b0b11b 🚀

---
+++ assets/chunks/vue.runtime.esm-bundler.CKu20WAY.js
@@ -0,0 +1,1 @@
+import{a as e,v as r}from"./theme.Y1eKPGrJ.js";import{v as a}from"./framework.7dHoyFAo.js";const t=e(r),o=e(a);export{o as a,t as r};
---
+++ assets/other_pcInstall_company.md.Bn55Y1aV.lean.js
@@ -0,0 +1,59 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/chunks/index.CcfjIN00.js","assets/chunks/theme.Y1eKPGrJ.js","assets/chunks/framework.7dHoyFAo.js","assets/chunks/vue.runtime.esm-bundler.CKu20WAY.js"])))=>i.map(i=>d[i]);
+import{aM as e,w as o,L as s,x as a,J as c,F as g,aX as _,bk as t,I as p,N as u,aZ as b,t as f,c5 as k}from"./chunks/framework.7dHoyFAo.js";import{I as y}from"./chunks/data.Bcf4_v_6.js";const E="/blog/images/daily.png",q="/blog/images/%E7%BD%91%E6%98%93%E7%81%B5%E7%8A%80%E5%8A%9E%E5%85%AC.png",P="/blog/images/daily-template.png",A="/blog/images/%E6%8A%A5%E9%94%80.png",I=JSON.parse('{"title":"入职须知","description":"","frontmatter":{"author":"张鑫","title":"入职须知","date":"2024/11/01","words":1491,"duration":12,"categories":["入职须知"],"tags":["入职须知"]},"headers":[],"relativePath":"other/pcInstall/company.md","filePath":"other/pcInstall/company.md","lastUpdated":1730513073000}'),N={name:"other/pcInstall/company.md"},L=Object.assign(N,{setup(v){const r=u(()=>k(()=>import("./chunks/index.CcfjIN00.js").then(l=>l.i),__vite__mapDeps([0,1,2,3])));return(l,n)=>{const i=b("BackTop"),m=b("ArticleMetadata"),d=b("MNavLinks");return e(),o("div",{"data-pagefind-body":!0},[s(i),n[0]||(n[0]=a("h1",{id:"入职须知",tabindex:"-1"},[c("入职须知 "),a("a",{class:"header-anchor",href:"#入职须知","aria-label":'Permalink to "入职须知"'},"​")],-1)),s(m,{article:{}}),n[1]||(n[1]=a("h2",{id:"日常办公沟通",tabindex:"-1"},[c("日常办公沟通 "),a("a",{class:"header-anchor",href:"#日常办公沟通","aria-label":'Permalink to "日常办公沟通"'},"​")],-1)),(e(!0),o(g,null,_(t(y),({title:x,items:h})=>(e(),f(d,{title:x,items:h},null,8,["title","items"]))),256)),n[2]||(n[2]=p('<h2 id="📰-日报-由-表格-和-邮箱-组成" tabindex="-1">📰 日报 (由 <strong><code>表格</code></strong> 和 <strong><code>邮箱</code></strong> 组成) <a class="header-anchor" href="#📰-日报-由-表格-和-邮箱-组成" aria-label="Permalink to &quot;📰 日报 (由 **`表格`** 和 **`邮箱`** 组成)&quot;">​</a></h2><h4 id="nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-1-表格如下-命名格式和内容如下" tabindex="-1">            1. 表格如下 (命名格式和内容如下) <a class="header-anchor" href="#nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-1-表格如下-命名格式和内容如下" aria-label="Permalink to &quot;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; 1. 表格如下 (命名格式和内容如下)&quot;">​</a></h4><img src="'+E+'" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]">',3)),s(t(r),{src:"/excel/工作汇报-xx-20241101.xlsx",style:{height:"100vh"},class:"w-[calc(100%_-_40px)] ml-[40px]"}),n[3]||(n[3]=p('<h4 id="nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-2-邮箱如下-邮箱需找-总经办龚艳-申请-注意-添加附件" tabindex="-1">            2. 邮箱如下 (邮箱需找 <strong><code>总经办龚艳</code></strong> 申请，注意 <strong><code>添加附件</code></strong>) <a class="header-anchor" href="#nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-2-邮箱如下-邮箱需找-总经办龚艳-申请-注意-添加附件" aria-label="Permalink to &quot;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; 2. 邮箱如下 (邮箱需找 **`总经办龚艳`** 申请，注意 **`添加附件`**)&quot;">​</a></h4><img src="'+q+'" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]"><img src="'+P+`" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]"><pre>    领导，您好：
+
+      以下是我今天工作内容的简述：
+
+      今日工作回顾：
+         《 上午 》：
+            1&gt;
+            2&gt;
+
+         《 下午 》：
+            1&gt;
+            2&gt;
+
+      以下是明天的工作事项简述：
+
+          明日工作计划：
+           1&gt;
+
+      详细的工作报告及相关文件已整理在 附件 中，请您查收。
+          如有任何疑问或需要进一步的信息，请随时与我联系。
+
+      此致
+      敬礼！
+
+      [姓名] 张鑫
+      [职位] 前端工程师
+      [日期]
+
+      联系方式：
+
+      手机：
+      邮箱：xxx@ningxiache.freeqiye.com
+
+      感谢您的关注与支持，期待与您共同推动项目的进展。
+</pre><h2 id="💰-报销" tabindex="-1">💰 报销 <a class="header-anchor" href="#💰-报销" aria-label="Permalink to &quot;💰 报销&quot;">​</a></h2><img src="`+A+'" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]">',6)),s(t(r),{src:"/excel/报销申请单（10月）.xlsx",style:{height:"100vh"},class:"w-[calc(100%_-_40px)] ml-[40px] my-[30px]"}),n[4]||(n[4]=p(`<h2 id="✅-打卡" tabindex="-1">✅ 打卡 <a class="header-anchor" href="#✅-打卡" aria-label="Permalink to &quot;✅ 打卡&quot;">​</a></h2><pre>
+
+    1：<b class="text-[red]">打卡🐔打卡：</b>用钉钉把自己的人脸录入，这样就可以用打卡机人脸识别打卡；
+
+       <b class="text-[red]">蓝牙打卡：</b>也可以手机打开蓝牙，考勤机 5-10米内 都可连接；
+
+    2：每人每月 <b class="text-[red]">只有1次补卡机会，</b>大家切记不要忘记打卡；
+
+    3：<b class="text-[red]">加班的同事也一定不要忘记打卡，</b>不然不好统计加班时长；
+
+    4：每月 <b class="text-[red]">5号之前</b> 请大家整理一下自己 <b class="text-[red]">上月需要报销的单据，</b> 需要报销单的可以找 <b class="text-[red]">吴芳</b> 领；
+
+    5: 如果当月有 <b class="text-[red]">1</b> 次 打卡 <b class="text-[red]">异常</b> (包括缺卡、迟到)，都可 <b class="text-[red]">补卡</b>；
+
+        第 <b class="text-[red]">2</b> 次迟到，扣除 <b class="text-[red]">¥20 + 全勤奖</b>
+
+        第 <b class="text-[red]">3</b> 次迟到，扣除 <b class="text-[red]">¥40 + 全勤</b>
+
+        第 <b class="text-[red]">4</b> 次迟到，扣除 <b class="text-[red]">3天工资 + 全勤</b>
+
+    6: 每个月 5 号之前，吴芳 会将 <b class="text-[red]">上个月考勤</b> 统计好，发至群里，请大家仔细核对。
+
+        如有异议，请联系 <b class="text-[red]">吴芳</b>。
+</pre>`,2))])}}});export{I as __pageData,L as default};
