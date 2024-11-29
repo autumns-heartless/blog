418e0537bfb491ca64df02c3a9ce7b0cc18da986
autumns-heartless <38639760+autumns-heartless@users.noreply.github.com>
2024-11-29 15:02:52 +0800

    Deploying to gh-pages from @ autumns-heartless/blog@01e7195d963471df1a6a88ca197da2855319b4cc 🚀

---
+++ assets/other_pcInstall_company.md.DdzZrV87.lean.js
@@ -0,0 +1,62 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/chunks/index.D0OeCgpi.js","assets/chunks/theme.Bes2BStD.js","assets/chunks/framework.D69h5UT5.js","assets/chunks/vue.runtime.esm-bundler.BIk8cs63.js"])))=>i.map(i=>d[i]);
+import{aM as l,w as c,L as a,x as n,J as d,F as _,aX as u,bk as e,I as t,N as w,aZ as r,t as f,c5 as q}from"./chunks/framework.D69h5UT5.js";import{_ as p}from"./chunks/theme.Bes2BStD.js";import{I as E}from"./chunks/data.BhbD52L3.js";const k="/blog/images/%E6%9C%88%E5%BA%A6%E8%BF%B0%E8%81%8C%E8%A1%A8.png",v="/blog/images/daily.png",y="/blog/images/%E7%BD%91%E6%98%93%E7%81%B5%E7%8A%80%E5%8A%9E%E5%85%AC.png",A="/blog/images/daily-template.png",P="/blog/images/%E6%8A%A5%E9%94%80.png",V=JSON.parse('{"title":"入职须知","description":"","frontmatter":{"author":"张鑫","title":"入职须知","date":"2024/11/01","words":1491,"duration":12,"categories":["入职须知"],"tags":["入职须知"]},"headers":[],"relativePath":"other/pcInstall/company.md","filePath":"other/pcInstall/company.md","lastUpdated":1730516073000}'),B={name:"other/pcInstall/company.md"},C=Object.assign(B,{setup(N){const b=w(()=>q(()=>import("./chunks/index.D0OeCgpi.js").then(o=>o.i),__vite__mapDeps([0,1,2,3])));return(o,s)=>{const i=r("BackTop"),m=r("ArticleMetadata"),x=r("MNavLinks");return l(),c("div",{"data-pagefind-body":!0},[a(i),s[0]||(s[0]=n("h1",{id:"入职须知",tabindex:"-1"},[d("入职须知 "),n("a",{class:"header-anchor",href:"#入职须知","aria-label":'Permalink to "入职须知"'},"​")],-1)),a(m,{article:{}}),s[1]||(s[1]=n("h2",{id:"👨‍⚖️-办公沟通",tabindex:"-1"},[d("👨‍⚖️ 办公沟通 "),n("a",{class:"header-anchor",href:"#👨‍⚖️-办公沟通","aria-label":'Permalink to "👨‍⚖️ 办公沟通"'},"​")],-1)),(l(!0),c(_,null,u(e(E),({title:g,items:h})=>(l(),f(x,{title:g,items:h},null,8,["title","items"]))),256)),s[2]||(s[2]=t(`<h2 id="💪-述职" tabindex="-1">💪 述职 <a class="header-anchor" href="#💪-述职" aria-label="Permalink to &quot;💪 述职&quot;">​</a></h2><h3 id="nbsp-nbsp-nbsp-nbsp-1-nbsp-5-天述职" tabindex="-1">     1 &gt;   <b class="text-[red]">5 天述职</b> <a class="header-anchor" href="#nbsp-nbsp-nbsp-nbsp-1-nbsp-5-天述职" aria-label="Permalink to &quot;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; 1 &gt; &amp;nbsp; &lt;b class=&quot;text-[red]&quot;&gt;5 天述职&lt;/b&gt;&quot;">​</a></h3><pre>
+
+      线下表格，<b class="text-[red]">龚燕</b> 会给你发放(纸质版)，5天之后 <b class="text-[red]">面向朱总</b> 进行述职
+</pre><h3 id="nbsp-nbsp-nbsp-nbsp-2-nbsp-月度述职" tabindex="-1">     2 &gt;   <b class="text-[red]">月度述职</b> <a class="header-anchor" href="#nbsp-nbsp-nbsp-nbsp-2-nbsp-月度述职" aria-label="Permalink to &quot;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; 2 &gt; &amp;nbsp; &lt;b class=&quot;text-[red]&quot;&gt;月度述职&lt;/b&gt;&quot;">​</a></h3><a href="/excel/月度述职表.xlsx" download="月度述职表.xlsx" target="view_window"><img src="`+p+'" class="download-svg"><img src="'+k+'" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]"></a>',5)),a(e(b),{src:"/excel/月度述职表.xlsx",style:{height:"100vh"},class:"w-[calc(100%_-_40px)] ml-[40px] mt-[20px]"}),s[3]||(s[3]=t('<h2 id="📰-日报-由-表格-和-邮箱-组成" tabindex="-1">📰 日报 (由 <strong><code>表格</code></strong> 和 <strong><code>邮箱</code></strong> 组成) <a class="header-anchor" href="#📰-日报-由-表格-和-邮箱-组成" aria-label="Permalink to &quot;📰 日报 (由 **`表格`** 和 **`邮箱`** 组成)&quot;">​</a></h2><h4 id="nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-1-表格如下-命名格式和内容如下" tabindex="-1">            1. 表格如下 (命名格式和内容如下) <a class="header-anchor" href="#nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-1-表格如下-命名格式和内容如下" aria-label="Permalink to &quot;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; 1. 表格如下 (命名格式和内容如下)&quot;">​</a></h4><a href="/excel/工作汇报-xx-20241101.xlsx" download="工作汇报-xx-20241101.xlsx" target="view_window"><img src="'+p+'" class="download-svg"><img src="'+v+'" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]"></a>',3)),a(e(b),{src:"/excel/工作汇报-xx-20241101.xlsx",style:{height:"100vh"},class:"w-[calc(100%_-_40px)] ml-[40px]"}),s[4]||(s[4]=t('<h4 id="nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-2-邮箱如下-邮箱需找-总经办龚艳-申请-注意-添加附件" tabindex="-1">            2. 邮箱如下 (邮箱需找 <strong><code>总经办龚艳</code></strong> 申请，注意 <strong><code>添加附件</code></strong>) <a class="header-anchor" href="#nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-nbsp-2-邮箱如下-邮箱需找-总经办龚艳-申请-注意-添加附件" aria-label="Permalink to &quot;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp;&amp;nbsp; 2. 邮箱如下 (邮箱需找 **`总经办龚艳`** 申请，注意 **`添加附件`**)&quot;">​</a></h4><a href="https://sirius-release.lx.netease.com/api/pub/client/update/download-windows" download="网易灵犀办公.exe" target="view_window"><img src="'+p+'" class="download-svg"><img src="'+y+'" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]"></a><img src="'+A+`" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]"><pre>    领导，您好：
+
+      以下是我今天工作内容的简述：
+
+      今日工作回顾：
+         《 上午 》：
+            1&gt;
+            2&gt;
+
+         《 下午 》：
+            1&gt;
+            2&gt;
+
+      以下是明天的工作事项简述：
+
+          明日工作计划：
+           1&gt;
+
+      详细的工作报告及相关文件已整理在 附件 中，请您查收。
+          如有任何疑问或需要进一步的信息，请随时与我联系。
+
+      此致
+      敬礼！
+
+      [姓名] xxx
+      [职位] 前端工程师
+      [日期]
+
+      联系方式：
+
+      手机：
+      邮箱：xxx@ningxiache.freeqiye.com
+
+      感谢您的关注与支持，期待与您共同推动项目的进展。
+</pre><h2 id="💰-报销" tabindex="-1">💰 报销 <a class="header-anchor" href="#💰-报销" aria-label="Permalink to &quot;💰 报销&quot;">​</a></h2><a href="/excel/报销申请单（10月）.xlsx" download="报销申请单（10月）.xlsx" target="view_window"><img src="`+p+'" class="download-svg"><img src="'+P+'" class="my-[30px] w-[calc(100%_-_40px)] ml-[40px]"></a>',6)),a(e(b),{src:"/excel/报销申请单（10月）.xlsx",style:{height:"100vh"},class:"w-[calc(100%_-_40px)] ml-[40px] my-[30px]"}),s[5]||(s[5]=t(`<h2 id="✅-打卡" tabindex="-1">✅ 打卡 <a class="header-anchor" href="#✅-打卡" aria-label="Permalink to &quot;✅ 打卡&quot;">​</a></h2><pre>
+
+    1：<b class="text-[red]">打卡🐔打卡：</b>用钉钉把自己的人脸录入，这样就可以用打卡机人脸识别打卡；
+
+       <b class="text-[red]">蓝牙打卡：</b>也可以手机打开蓝牙，考勤机 5-10米内 都可连接；
+
+    2：每人每月 <b class="text-[red]">只有1次补卡机会，</b>大家切记不要忘记打卡；
+
+    3：<b class="text-[red]">加班的同事也一定不要忘记打卡，</b>不然不好统计加班时长；
+
+    4：每月 <b class="text-[red]">5号之前</b> 请大家整理一下自己 <b class="text-[red]">上月需要报销的单据，</b> 需要报销单的可以找 <b class="text-[red]">吴芳</b> 领；
+
+    5: 如果当月有 <b class="text-[red]">1</b> 次 打卡 <b class="text-[red]">异常</b> (包括缺卡、迟到)，都可 <b class="text-[red]">补卡</b>；
+
+        第 <b class="text-[red]">2</b> 次迟到，扣除 <b class="text-[red]">¥20 + 全勤奖</b>
+
+        第 <b class="text-[red]">3</b> 次迟到，扣除 <b class="text-[red]">¥40 + 全勤</b>
+
+        第 <b class="text-[red]">4</b> 次迟到，扣除 <b class="text-[red]">3天工资 + 全勤</b>
+
+    6: 每个月 5 号之前，吴芳 会将 <b class="text-[red]">上个月考勤</b> 统计好，发至群里，请大家仔细核对。
+
+        如有异议，请联系 <b class="text-[red]">吴芳</b>。
+</pre>`,2))])}}});export{V as __pageData,C as default};
