ab5b388b065910b956d3155dc7cbdc08fe404c87
autumns-heartless <38639760+autumns-heartless@users.noreply.github.com>
2025-03-06 11:33:10 +0800

    Deploying to gh-pages from @ autumns-heartless/blog@31e2b5558e7ef2c25e700982b2490d2590c25812 🚀

---
+++ assets/todoList_index.md.zyRN5cGb.js
@@ -0,0 +1,30 @@
+import{_ as i,B as t,c as l,o as d,G as a,j as n,a3 as r,a as c}from"./chunks/framework.BDeXix0C.js";const T=JSON.parse('{"title":"待办事项 - 待开发","description":"","frontmatter":{},"headers":[],"relativePath":"todoList/index.md","filePath":"todoList/index.md","lastUpdated":1740447022000}'),_={name:"todoList/index.md"};function p(m,e,b,x,k,u){const s=t("BackTop"),o=t("ArticleMetadata");return d(),l("div",{"data-pagefind-body":!0},[a(s),e[0]||(e[0]=n("h1",{id:"待办事项-待开发",tabindex:"-1"},[c("待办事项 - 待开发 "),n("a",{class:"header-anchor",href:"#待办事项-待开发","aria-label":'Permalink to "待办事项 - 待开发"'},"​")],-1)),a(o,{article:{}}),e[1]||(e[1]=r(`<br><details class="details custom-block"><summary>待办事项选用</summary><ul class="task-list"><li class="task-list-item"><input type="checkbox" id="cbx_0" disabled="true"><label for="cbx_0">   markdown-it-task-checkbox</label></li><li class="task-list-item"><input type="checkbox" id="cbx_1" checked="true" disabled="true"><label for="cbx_1">   emoji</label></li></ul></details><pre class="mt-10">  ✅ 添加网站音乐播放
+
+  ✅ 待办事项 UI 优化
+
+  ✅ 目前为搜索为 pagefind
+
+  ✅ 博客添加分类标签
+
+  ✅ 团队成员墙 UI 优化 v1.0
+
+  ✅ 添加 RSS 源，⬜ 并研究出其用意
+
+  ✅ 媳妇儿白酒销路打开第 1 步：广告
+
+  ⬜ 媳妇儿白酒销路打开第 2 步：外部网站
+
+  ⬜ 代码规范建设：如 try
+
+  ⬜ 其他成员模块和笔记编写规划
+
+  ⬜ 笔记分类规划
+
+  ⬜ live2d 实现拖拽
+
+  ⬜ 导航实现置顶网站栏目展示
+
+  ⬜ 实现个人炫酷简历
+  
+  ⬜ 工作月度大屏模板
+</pre>`,3))])}const h=i(_,[["render",p]]);export{T as __pageData,h as default};
